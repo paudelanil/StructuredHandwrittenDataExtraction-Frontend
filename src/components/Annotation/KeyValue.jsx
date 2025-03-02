6f005d8488adaeae2981ae99260e8fad65e3a8bf
paudelanil <paudelanil9@gmail.com>
2025-03-02 10:21:12 +0545

    Auto reisizable text field

--- src/components/Annotation/KeyValue.jsx
+++ src/components/Annotation/KeyValue.jsx
@@ -69,9 +69,89 @@
 //   );
 // }
 
+// import * as React from "react";
+// import Box from "@mui/material/Box";
+// import TextField from "@mui/material/TextField";
+// import CancelOutlinedIcon from "@mui/icons-material/CancelOutlined";
+// import IconButton from "@mui/material/IconButton";
+// import InputAdornment from "@mui/material/InputAdornment";
+// import "./keyvalue.scss";
+
+// export default function BasicTextFields({
+//   data,
+//   setActiveInput,
+//   setData,
+//   setArrowButtonDisabled,
+//   firstLabelInputRef,
+// }) {
+//   return (
+//     <Box
+//       component="form"
+//       sx={{
+//         "& > :not(style)": { my: 2, width: "45ch" },
+//       }}
+//       noValidate
+//       autoComplete="off"
+//     >
+//       {Object.keys(data).map((key, index) => {
+//         if (key === "bbox" || data[key].label_name === "O") {
+//           return null; // Skip rendering for bbox and O labels
+//         }
+//         return (
+//           <div className="kv-label" key={key}>
+//             <TextField
+//               onClick={() => {
+//                 setArrowButtonDisabled(true);
+//               }}
+//               inputRef={index === 0 ? firstLabelInputRef : null}
+//               key={key}
+//               onFocus={() => setActiveInput(key)}
+//               value={data[key].text}
+//               id={key}
+//               label={data[key].label_name}
+//               variant="outlined"
+//               onChange={(e) => {
+//                 setData({
+//                   ...data,
+//                   [key]: {
+//                     ...data[key],
+//                     text: e.target.value,
+//                   },
+//                 });
+//               }}
+//               InputProps={{
+//                 endAdornment: (
+//                   <InputAdornment position="end">
+//                     <IconButton
+//                       aria-label="clear text"
+//                       edge="end"
+//                       onClick={() => {
+//                         setData({
+//                           ...data,
+//                           [key]: {
+//                             text: "",
+//                             label_name: data[key].label_name,
+//                             word_ids: [],
+//                           },
+//                         });
+//                       }}
+//                     >
+//                       <CancelOutlinedIcon />
+//                     </IconButton>
+//                   </InputAdornment>
+//                 ),
+//               }}
+//             />
+//           </div>
+//         );
+//       })}
+//     </Box>
+//   );
+// }
+
 import * as React from "react";
 import Box from "@mui/material/Box";
-import TextField from "@mui/material/TextField";
+import TextareaAutosize from "@mui/material/TextareaAutosize";
 import CancelOutlinedIcon from "@mui/icons-material/CancelOutlined";
 import IconButton from "@mui/material/IconButton";
 import InputAdornment from "@mui/material/InputAdornment";
@@ -95,11 +175,11 @@ export default function BasicTextFields({
     >
       {Object.keys(data).map((key, index) => {
         if (key === "bbox" || data[key].label_name === "O") {
-          return null; // Skip rendering for bbox and O labels
+          return null;
         }
         return (
           <div className="kv-label" key={key}>
-            <TextField
+            <TextareaAutosize
               onClick={() => {
                 setArrowButtonDisabled(true);
               }}
@@ -108,8 +188,9 @@ export default function BasicTextFields({
               onFocus={() => setActiveInput(key)}
               value={data[key].text}
               id={key}
-              label={data[key].label_name}
-              variant="outlined"
+              placeholder={data[key].label_name} // Use placeholder for label
+              minRows={3} // Set a minimum number of rows
+              style={{ width: "100%", padding: "8px", boxSizing: "border-box" }} // Basic styling
               onChange={(e) => {
                 setData({
                   ...data,
@@ -119,29 +200,23 @@ export default function BasicTextFields({
                   },
                 });
               }}
-              InputProps={{
-                endAdornment: (
-                  <InputAdornment position="end">
-                    <IconButton
-                      aria-label="clear text"
-                      edge="end"
-                      onClick={() => {
-                        setData({
-                          ...data,
-                          [key]: {
-                            text: "",
-                            label_name: data[key].label_name,
-                            word_ids: [],
-                          },
-                        });
-                      }}
-                    >
-                      <CancelOutlinedIcon />
-                    </IconButton>
-                  </InputAdornment>
-                ),
-              }}
             />
+            <IconButton
+              aria-label="clear text"
+              edge="end"
+              onClick={() => {
+                setData({
+                  ...data,
+                  [key]: {
+                    text: "",
+                    label_name: data[key].label_name,
+                    word_ids: [],
+                  },
+                });
+              }}
+            >
+              <CancelOutlinedIcon />
+            </IconButton>
           </div>
         );
       })}
